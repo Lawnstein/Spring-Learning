b5b6b70d5e692767ee11c25ee8c20b73cf8bf6fc
chenyong <chenyong@csii.com.cn>
2018-07-17 15:08:44 +0800

    add netty-tcp

---
+++ spring-netty-tcp/src/main/java/com/iceps/spring/assemble/Application.java
@@ -0,0 +1,45 @@
+package com.iceps.spring.assemble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+import com.iceps.spring.assemble.service.TestActionService;
+
+@SpringBootApplication
+@EnableCaching
+@ImportResource({ "classpath*:applicationContext.xml" })
+public class Application {
+	private static ApplicationContext applicationContext;
+
+	private static void testAssembleRing() {
+		com.iceps.spring.assemble.service.TestActionService service = (TestActionService) applicationContext
+				.getBean("assembleRing");
+		service.startThreads(false);
+	}
+
+	private static void testAssembleQueue() {
+		com.iceps.spring.assemble.service.TestActionService service = (TestActionService) applicationContext
+				.getBean("assembleQueue");
+		service.startThreads(false);
+	}
+
+	private static void testSift() {
+		com.iceps.spring.assemble.service.TestActionService service = (TestActionService) applicationContext
+				.getBean("sift");
+		service.startThreads(true);
+	}
+
+	public static void main(String[] args) {
+		applicationContext = SpringApplication.run(Application.class, args);
+		int l = 1;
+		for (int i = 0; i < l; i++) {
+			testAssembleRing();
+			testAssembleQueue();
+			testSift();
+		}
+	}
+
+}
